0a4ed7cc6465896373e2c13cff074e5e38b42d58
luauhza20-sudo <luauhza20@gmail.com>
2026-05-06 12:50:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -112,5 +112,6 @@ def analyze_route():
     result = analyze(text)
     return jsonify({"result": result})
 
-if __name__ == "__main__":
-    app.run(debug=False, port=5000)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
